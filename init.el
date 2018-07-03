db2e071a84f511579abc0f55aed5442b2f0cc342
Chen Pei Hao <s894117@gmail.com>
2018-07-03 14:40:14 +0900

    renew for macos-config

--- init.el
+++ init.el
@@ -22,14 +22,15 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (neotree exec-path-from-shell auctex better-shell nyan-mode org-gcal web-mode iedit expand-region jedi elpy flycheck htmlize ox-reveal company-mode counsel swiper ace-window monokai-theme smartparens hungry-delete org-bullets which-key try use-package))))
+    (company hungry-delete swiper counsel smartparens exec-path-from-shell monokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-(org-babel-load-file (expand-file-name "~/.emacs.d/setting.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/capture.org"))
+;; (org-babel-load-file (expand-file-name "~/.emacs.d/setting.org")) 
+;; (org-babel-load-file (expand-file-name "~/.emacs.d/capture.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/macos-config.org"))
 (load-file "~/.emacs.d/misc/wc.el")
 (put 'set-goal-column 'disabled nil)
