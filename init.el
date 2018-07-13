5ede7ab3b6f8b02084be2ffeb7224ec155f6eb30
Chen Pei Hao <s894117@gmail.com>
2018-07-13 11:41:10 +0900

    delete the cmake-ide

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit company hungry-delete swiper counsel smartparens exec-path-from-shell monokai-theme))))
+    (cmake-ide magit company hungry-delete swiper counsel smartparens exec-path-from-shell monokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
